86737e807def7c00ee1ddfa0814e037c1b14683b
Computer-Science-EAL <sist@eal.dk>
2018-02-19 16:18:40 +0100

    Update SunnyOrNot.py

--- Workshop2/SunnyOrNot.py
+++ Workshop2/SunnyOrNot.py
@@ -5,7 +5,10 @@ from sklearn import tree
 import numpy as np
 
 #1 = sunny, 2 = overcast, 3 = rainy
+#1 = grass, 2 = gravel
+
 #1 = yes, 0 = no
+
 #observations
 X_train = np.array([[1,2], [2,1], [3,1], [1,2], [1,2], [2,1], [3,1], [3,1], [1,2], [3,1], [1,1], [2,1], [2,1], [3,1], [3,2], [3,2], [3,1]]) #samples
 Y_train = np.array([0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0]) #classes
